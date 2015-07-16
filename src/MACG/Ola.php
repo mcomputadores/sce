9f91e20e515ac7273ed2dcbb01927235659e76a3
Marcos Gomes <marcospmsq@hotmail.com>
2015-07-16 18:02:47 -0300

    Commit inicial

---
+++ src/MACG/Ola.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MACG;
+
+
+/**
+ * Description of Ola
+ *
+ * @author User
+ */
+class Ola 
+{
+
+    public static function mundo()
+    {
+        echo "Olá Mundo!!!";
+        
+    }
+
+
+
+
+//put your code here
+}
